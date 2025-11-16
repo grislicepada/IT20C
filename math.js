0ca0c512b3c4dde34e0138dfe67c790443a8b726
grislicepada <graceliemaecepada3@gmail.com>
2025-11-17 07:52:39 +0800

    Added Quotient

--- math.js
+++ math.js
@@ -7,10 +7,15 @@ function diff(num1,num2){
     console.log(diff);
 }
 function product(num1,num2){
-    product = num1 - num2;
+    product = num1 * num2;
     console.log(product);
 }
 
+function quotient(num1,num2){
+    quotient = num1 / num2;
+    console.log(quotient);
+}
 sum(2,3);
 diff(5,3);
 product(17,5);
+quotient(6,7);
