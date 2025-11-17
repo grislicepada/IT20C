add8f2aaf007f118fb5c950ffbc1662af69b87e2
grislicepada <graceliemaecepada3@gmail.com>
2025-11-17 08:11:40 +0800

    Added if statement

---
+++ cs.js
@@ -0,0 +1,13 @@
+age = 18;
+
+function legalAge(age) {
+    if (age >= 18) {
+        console.log("Hala, guwang naka");
+    } else {
+        console.log("You are not an adult");
+
+    }
+}
+
+legalAge(50);
+legalAge(13);
