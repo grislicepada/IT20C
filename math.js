4b8e84cadfa37a5db0d9e5510df7aac2f16a2525
graxem_xpad4 <graceliemaecepada3@gmail.com>
2025-11-17 08:55:29 +0800

    Added ifelse update

--- math.js
+++ math.js
@@ -19,3 +19,13 @@ sum(2,3);
 diff(5,3);
 product(17,5);
 quotient(35,5);
+
+function quotient(num1,num2){
+
+    if(num2 === 0){
+        console.log("Cannot devide by zero");
+    }else{
+        quotient =num1 / num2;
+        console.log(quotient);
+    }
+}
