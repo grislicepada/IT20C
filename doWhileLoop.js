165acdf609a18b79e90f0f0be8ba8f296aa25527
grislicepada <graceliemaecepada3@gmail.com>
2025-11-20 08:56:03 +0800

    Added reverse doWHileLoop.js

--- doWhileLoop.js
+++ doWhileLoop.js
@@ -1,6 +1,30 @@
+/*
 i = 6;
 
 do{
     console.log(i);
     i++;
 } while (i<=5)
+ */
+
+// output = " ";
+// i = 1;
+
+// do{
+//     output += i + " ";
+
+//     i++;
+// } while(i<=5)
+
+//     console.log(output);
+
+
+output = " ";
+i = 5;
+
+do {
+    output += i + " ";
+    i--;
+} while (i>= 1)
+
+console.log(output);
