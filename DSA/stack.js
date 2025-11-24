8c781a8c60b208d75896deb2377028d3152b4a3d
grislicepada <graceliemaecepada3@gmail.com>
2025-11-24 08:53:56 +0800

    Added isEmpty and peek

--- DSA/stack.js
+++ DSA/stack.js
@@ -1,27 +1,38 @@
-class stack {
-    constructor() {
+class stack{
+    constructor(){
         this.data = {};
         this.topIndex = -1;
     }
 
-    traverse() {
+    traverse(){
         let output = "";
-        for (let i = 0; i <= this.topIndex; i++) {
+        for(let i=0; i<=this.topIndex; i++){
             output += this.data[i] + " ";
-            console.log(output);
+
         }
-        // console.log(output);
+        console.log(output);
     }
-    push(element) {
+    push(element){
         this.topIndex++;
         this.data[this.topIndex] = element;
 
     }
+
+    is_empty(){
+        return this.topIndex ===-1;
+    }
+    peek(){
+        //correcting statement
+        if(this.is_empty())return null;
+        return this.data[this.topIndex];
+
+        
+    }
 }
-let stack1 = new stack();
+let stack1 =  new stack();
 stack1.push(2);
-
 stack1.push(3);
-
 stack1.push(1);
 stack1.traverse()
+console.log("Is the stack empty? " + stack1.is_empty());
+console.log("Top element of the stack is " + stack1.peek());
