134b5eb6faad2db299ffeb35472b0656856c8b5f
grislicepada <graceliemaecepada3@gmail.com>
2025-11-24 08:48:04 +0800

    Added try lang edit

--- DSA/stack.js
+++ DSA/stack.js
@@ -1,27 +1,27 @@
-class stack{
-    constructor(){
+class stack {
+    constructor() {
         this.data = {};
         this.topIndex = -1;
     }
 
-    traverse(){
+    traverse() {
         let output = "";
-        for(let i=0; i<=this.topIndex; i++){
+        for (let i = 0; i <= this.topIndex; i++) {
             output += this.data[i] + " ";
-
+            console.log(output);
         }
-        console.log(output);
+        // console.log(output);
     }
-    push(element){
+    push(element) {
         this.topIndex++;
         this.data[this.topIndex] = element;
 
     }
 }
-let stack1 =  new stack();
+let stack1 = new stack();
 stack1.push(2);
-stack1.traverse()
+
 stack1.push(3);
-stack1.traverse()
+
 stack1.push(1);
 stack1.traverse()
